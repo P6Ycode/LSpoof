6f5c884bf5597beba7a53a96d28b93b2210ed26e
P6Ycode <thalightskintiller@gmail.com>
2026-07-21 11:11:05 -0400

    Complete shared location provider foundation

--- Source/LSLocationState.m
+++ Source/LSLocationState.m
@@ -1,5 +1,16 @@
 #import "LSLocationState.h"
 
+@interface LSLocationState ()
+@property (nonatomic, readwrite, getter=isSpoofingEnabled) BOOL spoofingEnabled;
+@property (nonatomic, readwrite) CLLocationCoordinate2D coordinate;
+@property (nonatomic, readwrite) CLLocationDirection heading;
+@property (nonatomic, readwrite) CLLocationSpeed speed;
+@property (nonatomic, readwrite) CLLocationDistance altitude;
+@property (nonatomic, readwrite) CLLocationAccuracy horizontalAccuracy;
+@property (nonatomic, readwrite) CLLocationAccuracy verticalAccuracy;
+@property (nonatomic, strong, readwrite) NSDate *timestamp;
+@end
+
 @implementation LSLocationState
 
 + (instancetype)shared {
@@ -9,33 +20,57 @@
         state = [[self alloc] init];
         state.coordinate = kCLLocationCoordinate2DInvalid;
         state.horizontalAccuracy = 5.0;
+        state.verticalAccuracy = 6.0;
         state.timestamp = [NSDate date];
     });
     return state;
 }
 
-- (CLLocation *)currentLocation {
-    if (!CLLocationCoordinate2DIsValid(self.coordinate)) {
-        return nil;
+- (nullable CLLocation *)currentLocation {
+    @synchronized (self) {
+        if (!self.spoofingEnabled || !CLLocationCoordinate2DIsValid(self.coordinate)) {
+            return nil;
+        }
+        return [[CLLocation alloc] initWithCoordinate:self.coordinate
+                                             altitude:self.altitude
+                                   horizontalAccuracy:self.horizontalAccuracy
+                                     verticalAccuracy:self.verticalAccuracy
+                                               course:self.heading
+                                                speed:self.speed
+                                            timestamp:self.timestamp ?: [NSDate date]];
     }
-
-    return [[CLLocation alloc] initWithCoordinate:self.coordinate
-                                         altitude:0
-                               horizontalAccuracy:self.horizontalAccuracy
-                                 verticalAccuracy:10
-                                           course:self.heading
-                                            speed:self.speed
-                                        timestamp:self.timestamp ?: [NSDate date]];
 }
 
 - (void)updateCoordinate:(CLLocationCoordinate2D)coordinate
-                heading:(CLLocationDirection)heading
-                 speed:(CLLocationSpeed)speed {
-    self.coordinate = coordinate;
-    self.heading = heading;
-    self.speed = speed;
-    self.timestamp = [NSDate date];
-    self.spoofingEnabled = YES;
+                 heading:(CLLocationDirection)heading
+                   speed:(CLLocationSpeed)speed
+                altitude:(CLLocationDistance)altitude
+      horizontalAccuracy:(CLLocationAccuracy)horizontalAccuracy
+        verticalAccuracy:(CLLocationAccuracy)verticalAccuracy {
+    if (!CLLocationCoordinate2DIsValid(coordinate)) {
+        [self clear];
+        return;
+    }
+    @synchronized (self) {
+        self.coordinate = coordinate;
+        self.heading = heading;
+        self.speed = MAX(0.0, speed);
+        self.altitude = altitude;
+        self.horizontalAccuracy = MAX(0.0, horizontalAccuracy);
+        self.verticalAccuracy = MAX(0.0, verticalAccuracy);
+        self.timestamp = [NSDate date];
+        self.spoofingEnabled = YES;
+    }
 }
 
+- (void)clear {
+    @synchronized (self) {
+        self.spoofingEnabled = NO;
+        self.coordinate = kCLLocationCoordinate2DInvalid;
+        self.heading = 0.0;
+        self.speed = 0.0;
+        self.altitude = 0.0;
+        self.timestamp = [NSDate date];
+    }
+}
 @end
