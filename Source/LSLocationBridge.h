411b662d836e4f9fa559e6adb18a0e559b3490be
P6Ycode <thalightskintiller@gmail.com>
2026-07-21 11:13:27 -0400

    Integrate realistic driving speed into shared provider

--- Source/LSLocationBridge.h
+++ Source/LSLocationBridge.h
@@ -2,6 +2,6 @@
 #import <CoreLocation/CoreLocation.h>
 
 NS_ASSUME_NONNULL_BEGIN
-FOUNDATION_EXPORT nullable CLLocation *LSGetCurrentSpoofedLocation(void);
+FOUNDATION_EXPORT CLLocation * _Nullable LSGetCurrentSpoofedLocation(void);
 FOUNDATION_EXPORT void LSClearCurrentSpoofedLocation(void);
 NS_ASSUME_NONNULL_END
