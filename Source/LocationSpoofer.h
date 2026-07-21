30ab9fa12853b5a2a83697279781627469913608
P6Ycode <thalightskintiller@gmail.com>
2026-07-21 11:11:14 -0400

    Complete shared location provider foundation

--- Source/LocationSpoofer.h
+++ Source/LocationSpoofer.h
@@ -2,15 +2,11 @@
 #import <CoreLocation/CoreLocation.h>
 
 NS_ASSUME_NONNULL_BEGIN
-
-FOUNDATION_EXPORT CLLocation *LSCreateSpoofedLocation(void);
+FOUNDATION_EXPORT nullable CLLocation *LSCreateSpoofedLocation(void);
 FOUNDATION_EXPORT BOOL LSIsInternalLocationCreate(void);
 FOUNDATION_EXPORT void LSSetHooksBypassed(BOOL bypassed);
 
 @interface LocationSpoofer : NSObject
-
 + (void)installHooks;
-
 @end
-
 NS_ASSUME_NONNULL_END
