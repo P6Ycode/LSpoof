59796734301ce3ed6a064b7cef0df2178318c086
P6Ycode <thalightskintiller@gmail.com>
2026-07-21 11:25:09 -0400

    Add rootless system-wide tweak target

---
+++ Source/LSSystemWideBridge.h
@@ -0,0 +1,22 @@
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef struct {
+    BOOL active;
+    BOOL paused;
+    CLLocationCoordinate2D coordinate;
+    CLLocationDirection heading;
+    CLLocationSpeed speed;
+    pid_t publisherPID;
+} LSLiveRouteSnapshot;
+
+FOUNDATION_EXPORT void LSPublishLiveRoute(CLLocationCoordinate2D coordinate,
+                                          CLLocationDirection heading,
+                                          CLLocationSpeed speed,
+                                          BOOL paused);
+FOUNDATION_EXPORT BOOL LSReadLiveRoute(LSLiveRouteSnapshot *snapshot);
+FOUNDATION_EXPORT void LSClearLiveRoute(void);
+
+NS_ASSUME_NONNULL_END
